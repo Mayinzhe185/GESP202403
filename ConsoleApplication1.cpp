a8274f8c4dd599866dec79cf29f4152cc13c7ba5
Ma Yinzhe <mayinzhe123456@163.com>
2025-12-24 21:46:31 +0800

    初始化控制台应用程序项目，添加基本文件和配置

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    const int c=a;
+    vector<int> b(c, 0);
+    for(int i=0;i<=c;i++){
+        if(i==0){
+            b[i]=1;
+            continue;
+        }
+        else if(i==1){
+            b[i]=1;
+            continue;
+        }
+        else if(i==2){
+            b[i]=2;
+            continue;
+        }
+        else if(i==3){
+            b[i]=4;
+            continue;
+        }
+        else{
+            b[i]=b[i-1]+b[i-2]+b[i-3];
+        }
+    }
+    cout << b[c] << endl;
+    return 0;
+}
